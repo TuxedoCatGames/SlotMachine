d1f049d1c3237e39d3dd5267ab6e03c1b5fd410a
Bob Keifer <bobkeifer@lake-como.local>
2014-12-17 16:09:21 -0500

    ongoing project development

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,17 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Bob Keifer on 12/17/14.
+//  Copyright (c) 2014 BitFountain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
